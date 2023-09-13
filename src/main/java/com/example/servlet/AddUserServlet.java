785e48589d5d7d254653f271a83fe7207e7f020e
user.email <sunkar2710@gmail.com>
2023-09-13 11:56:14 +0600

    test Servlet Application

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,36 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        // Retrieve the parameters from the request
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        // Create a new user and save it in the Warehouse
+        User user = new User(firstName, lastName);
+        Warehouse warehouse = Warehouse.getInstance();
+        warehouse.addUser(user);
+
+        // Set the "user" attribute in the request
+        request.setAttribute("user", warehouse.getUsers());
+
+        // Forward the request to the "/add" page
+        try {
+            request.getRequestDispatcher("/jsp/add.jsp").forward(request, response);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,32 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        // Retrieve the set of users from the Warehouse
+        Set<User> users = Warehouse.getInstance().getUsers();
+
+        // Set the "users" attribute in the request
+        request.setAttribute("users", users);
+
+        // Forward the request to the "/users" page
+        try {
+            request.getRequestDispatcher("/jsp/users.jsp").forward(request, response);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
